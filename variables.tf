4476d373d90f93cb0b0d27a908937a75247af9af
Yashwanth Bachupally <67672484+YashwanthBachupally@users.noreply.github.com>
2025-01-06 13:16:41 +0530

    spot ec2 instance with tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI ID
+}
+
+variable "spot_price" {
+  description = "Maximum spot price"
+  type        = string
+  default     = "0.0035" # Set your maximum spot price
+}
